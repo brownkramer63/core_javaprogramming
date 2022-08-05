ca949da942013e5ed97500a991e4c6d59256af6f
12177 <brownkramer63@gmail.com>
2022-08-05 17:04:33 -0500

    ozzy project day

--- src/StaySharp/StringEndsWith.java
+++ src/StaySharp/StringEndsWith.java
@@ -1,13 +1,16 @@
 package StaySharp;
 
 public class StringEndsWith {
+    public static void main(String[] args) {
+        System.out.println(solution("samurai", "ai"));
+    }
 
-//        public static boolean solution(String str, String ending) {
-//            String strend = str.substring(str.length()-ending.length());
-//            if(strend.equals(ending)){
-//                return true;
-//            }else{
-//                return false;
-//            }  }
-
+    public static boolean solution(String str, String ending) {
+        String strend = str.substring( (str.length()-1)-(ending.length()-1),str.length() );
+        System.out.println(strend);
+        if(strend.equals(ending)){
+            return true;
+        }else{
+            return false;
+        }  }
 }
