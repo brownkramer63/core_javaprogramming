6c97bb8020f0008d364e2c57ae395158eec29fa0
12177 <brownkramer63@gmail.com>
2022-08-07 13:40:35 -0500

    stayingSharp

--- src/StaySharp/codewarsContin.java
+++ src/StaySharp/codewarsContin.java
@@ -1,4 +1,20 @@
 package StaySharp;
 
 public class codewarsContin {
+    public static void main(String[] args) {
+
+        System.out.println(doubleChar("kramer"));
+    }
+
+    public static String doubleChar(String str) {
+        String s ="";
+        for(int i=0; i<str.length(); i++){
+
+            String f=str.charAt(i)+""+str.charAt(i)+"";
+            s=s+f;
+
+        }
+return s;
+    }
+
 }
